81b1ca01f9f44f1663358e6faafdd9c8f793728d
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-22 23:10:05 +0900

    refactor: 함수형 파라미터 넘겨주기 -> 객체의 상태를 활용

--- src/main/kotlin/racingcar/Game/RacingCar.kt
+++ src/main/kotlin/racingcar/Game/RacingCar.kt
@@ -1,8 +1,6 @@
 package racingcar.Game
 
-import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import racingcar.Validation.Validation
 import racingcar.View.InputView
 import racingcar.View.OutputView
 
@@ -10,41 +8,39 @@ class RacingCar {
     private val outputView = OutputView()
     private val inputView = InputView()
 
-    val validation = Validation()
-
-    fun carsNameTrim(cars : List<String>): List<String> {
-        return cars.map { it.trim() }
-    }
+    private val movingOfCarsRecord = MutableList(cars.count()) { 0 }
+    lateinit var cars : List<String>
+    private var count : Int = 0
 
     fun isCanGo(): Boolean {
         return Randoms.pickNumberInRange(0, 9) >= 4
     }
 
-    fun gameOfOneCar(carsMoving: MutableList<Int>, index: Int) {
+    fun gameOfOneCar(index: Int) {
         if (isCanGo()) {
-            carsMoving[index] += 1
+            movingOfCarsRecord[index] += 1
         }
     }
 
-    fun gameOfTurn(cars: List<String>, movingOfCars: MutableList<Int>, outputView: OutputView) {
+    fun gameOfTurn() {
         for (i in 0 until cars.count()) {
-            gameOfOneCar(movingOfCars, i)
-            outputView.printGameResult(cars[i], movingOfCars[i])
+            gameOfOneCar(i)
+            outputView.printGameResult(cars[i], movingOfCarsRecord[i])
         }
         println()
     }
 
-    fun game(cars: List<String>, movingOfCars: MutableList<Int>, outputView: OutputView, count: Int) {
+    fun game() {
         for (i in 0 until count) {
-            gameOfTurn(cars, movingOfCars, outputView)
+            gameOfTurn()
         }
     }
 
-    fun getWinnerIndex(carsMoving: MutableList<Int>): MutableList<Int> {
+    fun getWinnerIndex(): MutableList<Int> {
         val winners = mutableListOf<Int>()
         var maxNumber = 0
 
-        carsMoving.forEachIndexed { idx, it ->
+        movingOfCarsRecord.forEachIndexed { idx, it ->
             if (it == maxNumber) {
                 winners.addLast(idx)
             }
@@ -58,14 +54,12 @@ class RacingCar {
     }
 
     fun start(){
-        val cars = inputView.inputCars()
-        val count = inputView.inputCount()
+        cars = inputView.inputCars()
+        count = inputView.inputCount()
 
-        val movingOfCars = MutableList(cars.count()) { 0 }
+        game()
 
-        game(cars, movingOfCars, outputView, count)
-
-        val winners = getWinnerIndex(movingOfCars).map { cars[it] }
+        val winners = getWinnerIndex().map { cars[it] }
         outputView.printOutput()
         outputView.printWinner(winners)
     }
